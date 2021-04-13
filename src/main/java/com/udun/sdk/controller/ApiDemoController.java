fb4cdd463ee6fbf00135aef4628d772ee31db52c
miller <scofeild806@gmail.com>
2021-04-13 18:00:43 +0800

    init

---
+++ src/main/java/com/udun/sdk/controller/ApiDemoController.java
@@ -0,0 +1,89 @@
+package com.udun.sdk.controller;
+
+import cn.hutool.json.JSONUtil;
+import com.udun.sdk.domain.Address;
+import com.udun.sdk.domain.Coin;
+import com.udun.sdk.domain.ResultMsg;
+import com.udun.sdk.service.UdunService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ApiDemoController {
+    Logger log = LoggerFactory.getLogger(ApiDemoController.class);
+    @Autowired
+    UdunService udunService;
+    private static final String SUCCESS = "success";
+
+    /**
+     * 生成地址
+     *
+     * @return 返回参数
+     */
+    @GetMapping("/create")
+    public String create() {
+        Address address = udunService.createAddress("0", "1", "1", "http://demo.com/notify");
+        log.info(JSONUtil.toJsonStr(address));
+        return SUCCESS;
+    }
+
+    /**
+     * 提币
+     *
+     * @return 返回参数
+     */
+    @GetMapping("/withdraw")
+    public String withdraw() {
+        ResultMsg resultMsg = udunService.withdraw("0xa09921e9a3886e1b2b79e8fcd27d3a61ebe0ecd9", BigDecimal.TEN,
+                "520", "520",
+                "42423121", "", "http://demo.com/notify");
+        log.info(resultMsg.toString());
+        return SUCCESS;
+    }
+
+    /**
+     * 代付
+     *
+     * @return 返回参数
+     */
+    @GetMapping("proxyPay")
+    public String proxyPay() {
+        ResultMsg resultMsg = udunService.proxyPay("0xa09921e9a3886e1b2b79e8fcd27d3a61ebe0ecd9", BigDecimal.TEN,
+                "520", "520",
+                "43242312321321", "", "http://demo.com/notify");
+        log.info(resultMsg.toString());
+        return SUCCESS;
+    }
+
+    /**
+     * 检验地址合法性
+     *
+     * @return 返回参数
+     */
+    @GetMapping("/checkAddress")
+    public String checkAddress() {
+        boolean b = udunService.checkAddress("520", "32423432");
+        log.info("{}", b);
+        return SUCCESS;
+    }
+
+    /**
+     * 获取商户支持币种
+     *
+     * @return 返回参数
+     */
+    @GetMapping("/listSupportCoin")
+    public String listSupportCoin() {
+        List<Coin> coinList = udunService.listSupportCoin(false);
+        log.info("{}", coinList.size());
+        return SUCCESS;
+    }
+}
---
+++ src/main/java/com/udun/sdk/controller/CallBackController.java
@@ -0,0 +1,59 @@
+package com.udun.sdk.controller;
+
+import cn.hutool.json.JSONUtil;
+import com.udun.sdk.config.UdunProperties;
+import com.udun.sdk.domain.Trade;
+import com.udun.sdk.util.UdunUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/back")
+public class CallBackController {
+    Logger log = LoggerFactory.getLogger(ApiDemoController.class);
+    @Autowired
+    UdunProperties udunProperties;
+
+    /**
+     * 处理优盾网关回调信息，包括充币和提币
+     *
+     * @param timestamp 时间戳
+     * @param nonce     随机数
+     * @param body      消息内容
+     * @param sign      签名
+     * @return 返回参数
+     */
+    @PostMapping("/notify")
+    public String tradeCallback(@RequestParam("timestamp") String timestamp,
+                                @RequestParam("nonce") String nonce,
+                                @RequestParam("body") String body,
+                                @RequestParam("sign") String sign) {
+        log.info("timestamp:{},nonce:{},sign:{},body:{}", timestamp, nonce, sign, body);
+        if (!UdunUtils.checkSign(udunProperties.getMerchantKey(), timestamp, nonce, body, sign)) {
+            return "error";
+        }
+        Trade trade = JSONUtil.toBean(body, Trade.class);
+        if (trade.getTradeType() == 1) {
+            log.info("充币回调处理");
+            //TODO
+        } else if (trade.getTradeType() == 2) {
+            log.info("提币回调处理");
+            if (trade.getStatus() == 1) {
+                log.info("审核通过");
+                //TODO
+            } else if (trade.getStatus() == 2) {
+                log.info("审核不通过");
+                //TODO
+            } else if (trade.getStatus() == 3) {
+                log.info("提币已到账");
+                //TODO
+            }
+        }
+        return "success";
+    }
+}
